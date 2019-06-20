f1ce4d424e9d990b5db5a0db60d636947d88ae7d
liuming_music <liuming_music@163.com>
2019-06-20 23:31:41 +0800

    脚手架升级

---
+++ src/test/java/com/example/guava/BasicTools.java
@@ -0,0 +1,19 @@
+package com.example.guava;
+
+
+/**
+ * 描述: 基础工具
+ *
+ * @Author liumm
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Date 2019-04-14 21:56
+ */
+public class BasicTools {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
